c0ccc2d7be3e1e56b4562ffd21d11fb9a76b935b
mrxdoffical <berogergesg@gmail.com>
2024-10-31 11:12:15 +0300

    task1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -9,7 +9,7 @@ from typing import List
 
 def index_range(page: int, page_size: int) -> tuple:
     """
-    Calculate the start and end index for a given
+    Calculate
 
     Args:
         page (int): The current page number (1-indexed).
@@ -20,28 +20,27 @@ def index_range(page: int, page_size: int) -> tuple:
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    return (start_index, end_index)
+    return start_index, end_index
 
 
 class Server:
-    """server class
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """cached database"""
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
-                reading = csv.reader(f)
-                dataset = [row for row in reading]
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
             self.__dataset = dataset[1:]
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> list[list]:
-        """Get paginated data"""
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get paginated data."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
@@ -50,4 +49,5 @@ class Server:
 
         if start_index >= len(dataset):
             return []
+
         return dataset[start_index:end_index]
