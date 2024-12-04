4995965e81eb13639f5fe81e37252d31561c79c0
mrxdoffical <berogergesg@gmail.com>
2024-12-04 18:44:29 +0200

    task6

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,29 @@
+import redis from 'redis';
+import kue from 'kue';
+
+const queue = kue.createQueue();
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+client.on('complete', () => console.log('Notification job completed'));
+
+client.on('falied', () => console.log('Notification job failed'));
+
+const jobData = {
+  phoneNumber: '01063440605',
+  message: 'This is the code to verify your account',
+};
+
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
