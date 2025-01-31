7c13f5f8899ee5bfb865d0aaaedbb62a55ce0fb1
mrxdoffical <berogergesg@gmail.com>
2025-01-31 15:10:21 +0200

    task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index() -> str:
     """
@@ -15,6 +16,7 @@ def index() -> str:
         str: Rendered HTML template for the index page.
     """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
