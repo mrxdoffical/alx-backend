b56a0751c6b6266d036dc4dc8620f4495ae9fcc0
mrxdoffical <berogergesg@gmail.com>
2025-01-31 16:38:33 +0200

    task4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""A Basic Flask app with Babel setup and locale selection.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Configuration for Babel."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determine the best match for supported languages."""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page."""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
